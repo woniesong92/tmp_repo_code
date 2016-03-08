65ab807efab9acec4d94543449ce5d30d3c9f930
Sol Kim <solkim@Sols-MacBook-Pro.local>
2016-03-08 14:44:03 -0500

    changes

--- wp-content/themes/onboardiq/template-parts/content.php
+++ wp-content/themes/onboardiq/template-parts/content.php
@@ -10,38 +10,45 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <div class="entry-meta">
-		<?php onboardiq_posted_on(); ?>
-    </div><!-- .entry-meta -->
-	<header class="entry-header">
-		<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
+  <div class="container">
+    <div class="row">
+      <div class=".col-md-5"></div>
+      <div class=".col-md-7 post-content-text">
+        <div class="entry-meta">
+            <?php onboardiq_posted_on(); ?>
+        </div><!-- .entry-meta -->
+        <header class="entry-header">
+            <?php
+                if ( is_single() ) {
+                    the_title( '<h1 class="entry-title">', '</h1>' );
+                } else {
+                    the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+                }
 
-		if ( 'post' === get_post_type() ) : ?>
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
+            if ( 'post' === get_post_type() ) : ?>
+            <?php
+            endif; ?>
+        </header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'onboardiq' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+        <div class="entry-content">
+            <?php
+                the_content( sprintf(
+                    /* translators: %s: Name of current post. */
+                    wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'onboardiq' ), array( 'span' => array( 'class' => array() ) ) ),
+                    the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                ) );
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'onboardiq' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+                wp_link_pages( array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'onboardiq' ),
+                    'after'  => '</div>',
+                ) );
+            ?>
+        </div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php onboardiq_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+        <footer class="entry-footer">
+            <?php onboardiq_entry_footer(); ?>
+        </footer><!-- .entry-footer -->
+        </div><!-- .col-md-7 -->
+      </div><!-- .row -->
+  </div><!-- .container -->
 </article><!-- #post-## -->
