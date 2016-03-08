c6a5a3d707bde827d18062b281dec4bb54b80bb1
Sol Kim <solkim@Sols-MacBook-Pro.local>
2016-03-08 15:50:16 -0500

    changes

--- wp-content/themes/onboardiq/template-parts/content-page.php
+++ wp-content/themes/onboardiq/template-parts/content-page.php
@@ -16,7 +16,7 @@
 
 	<div class="entry-content">
 		<?php
-			the_exerpt();
+			the_content();
 
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'onboardiq' ),
