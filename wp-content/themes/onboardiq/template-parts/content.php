ee371279873e8b3bc673b869350e37e0c87f4c6c
Sol Kim <solkim@Sols-MacBook-Pro.local>
2016-03-08 15:52:54 -0500

    changes

--- wp-content/themes/onboardiq/template-parts/content.php
+++ wp-content/themes/onboardiq/template-parts/content.php
@@ -32,7 +32,7 @@
 
         <div class="entry-content">
             <?php
-                the_exerpt( sprintf(
+                the_content( sprintf(
                     /* translators: %s: Name of current post. */
                     wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'onboardiq' ), array( 'span' => array( 'class' => array() ) ) ),
                     the_title( '<span class="screen-reader-text">"', '"</span>', false )
