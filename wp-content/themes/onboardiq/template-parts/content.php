08fb3de08d003a04bc51d5349d86a7f26407f71d
Sol Kim <solkim@Sols-MacBook-Pro.local>
2016-03-09 04:44:57 -0500

    changes

--- wp-content/themes/onboardiq/template-parts/content.php
+++ wp-content/themes/onboardiq/template-parts/content.php
@@ -20,7 +20,7 @@
         <div class="entry-meta">
             <?php onboardiq_posted_on(); ?>
             <p style="padding-right: 15px; padding-left: 15px">|</p>
-            <fb:comments-count href="<?php echo get_permalink($post->ID); ?>"></fb:comments-count> comments
+            <p><fb:comments-count href="<?php echo get_permalink($post->ID); ?>"></fb:comments-count> comments</p>
             <?php echo do_shortcode("[cresta-social-share]"); ?>
         </div><!-- .entry-meta -->
         <header class="entry-header">
